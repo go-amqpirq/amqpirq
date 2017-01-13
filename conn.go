3d65fef0d62410b6b69444daf0fcc2c0e137c301
mbogus <mbogus@users.noreply.github.com>
2017-01-13 10:57:36 +0000

    Added ParallelMessageListener implementation

--- conn.go
+++ conn.go
@@ -2,6 +2,8 @@ package amqpirq
 
 import (
 	"crypto/tls"
+	"errors"
+	"fmt"
 	"github.com/streadway/amqp"
 	"time"
 )
@@ -9,6 +11,21 @@ import (
 const (
 	defaultMaxAttempts = -1
 	defaultDelay       = uint(30)
+)
+
+var (
+	// NamedReplyQueue is a lambda for a amqp.Queue qn definition on
+	// amqp.Channel ch. The queue is defined as non-durable, non-exclusive
+	NamedReplyQueue = func(ch *amqp.Channel, qn string) (amqp.Queue, error) {
+		return ch.QueueDeclare(
+			qn,    // name
+			false, // durable
+			false, // delete when unused
+			false, // exclusive
+			false, // no-wait
+			nil,   // arguments
+		)
+	}
 )
 
 // MessageListener is enabling configuring channels and queues to implement
@@ -62,6 +79,7 @@ func dialFunc(dial func() (*amqp.Connection, error)) (*Connection, error) {
 
 }
 
+// Close sends a close signal to all MessageListener-s
 func (c *Connection) Close() {
 	select {
 	case c.ch <- new(struct{}):
@@ -121,4 +139,89 @@ func serveAttempt(dial func() (*amqp.Connection, error), closing chan *struct{},
 	<-forever
 
 	return <-exErr
+}
+
+// DeliveryConsumer is an interface for handling amqp.Delivery messages
+// consumed from amqp.Channel on a queue
+type DeliveryConsumer interface {
+	Consume(*amqp.Channel, *amqp.Delivery)
+}
+
+// ParallelMessageListener is a parallel and asynchronous implementation of
+// MessageListener
+type ParallelMessageListener struct {
+	consumer DeliveryConsumer
+	size     int
+	qn       string
+}
+
+// NewParallelMessageListener returns new MessageListener with a fixed pool
+// size - size for the queue qn. Inbound messages are processed using
+// DeliveryConsumer consumer.
+func NewParallelMessageListener(qn string, size int, consumer DeliveryConsumer) (MessageListener, error) {
+	if size < 1 {
+		return nil, errors.New("amqpirq: pool size is required")
+	}
+	if qn == "" {
+		return nil, errors.New("amqpirq: queue name is required")
+	}
+	return ParallelMessageListener{
+		consumer: consumer,
+		size:     size,
+		qn:       qn,
+	}, nil
+}
+
+func (ml ParallelMessageListener) Listen(conn *amqp.Connection, alive <-chan struct{}) {
+	ch, err := conn.Channel()
+	failOnError(err, "failed to open a channel")
+	defer ch.Close()
+
+	q, err := NamedReplyQueue(ch, ml.qn)
+	failOnError(err, "failed to declare a queue")
+
+	err = ch.Qos(
+		ml.size,
+		0,
+		false,
+	)
+	failOnError(err, "failed to set QoS")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "failed to register a consumer")
+
+	wrkCh := make(chan amqp.Delivery, ml.size)
+
+	for i := 0; i < ml.size; i++ {
+		go func() {
+			for {
+				select {
+				case d := <-wrkCh:
+					ml.consumer.Consume(ch, &d)
+				case <-alive:
+					// avoid leaking goroutines
+					return
+				}
+			}
+		}()
+	}
+
+	for d := range msgs {
+		wrkCh <- d
+	}
+	<-alive
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		panic(fmt.Errorf("amqpirq: %s: %v", msg, err))
+	}
 }
--- conn_test.go
+++ conn_test.go
@@ -2,8 +2,10 @@ package amqpirq
 
 import (
 	"github.com/streadway/amqp"
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 )
 
 type dummyListener struct {
@@ -78,17 +80,17 @@ func TestConnection_Listen(t *testing.T) {
 	c.MaxAttempts = 1
 	c.Delay = 0
 
-	l := new(dummyListener)
-	go c.Listen(l)
+	listener := new(dummyListener)
+	go c.Listen(listener)
 
 	for {
-		if l.started {
+		if listener.started {
 			break
 		}
 	}
 	c.Close()
 	for {
-		if l.ended {
+		if listener.ended {
 			break
 		}
 	}
@@ -107,4 +109,103 @@ func TestConnection_ListenInvalidURI(t *testing.T) {
 	}
 }
 
+func TestNewParallelMessageListener_InvalidSize(t *testing.T) {
+	_, err := NewParallelMessageListener("", 0, nil)
+	if err == nil {
+		t.Fatal("Expected error, got <nil>")
+	}
+}
+
+func TestNewParallelMessageListener_MissingQueue(t *testing.T) {
+	_, err := NewParallelMessageListener("", 1, nil)
+	if err == nil {
+		t.Fatal("Expected error, got <nil>")
+	}
+}
+
+type dummyDeliveryConsumer struct {
+	corrID *string
+}
+
+func (c *dummyDeliveryConsumer) Consume(ch *amqp.Channel, d *amqp.Delivery) {
+	c.corrID = &d.CorrelationId
+	d.Ack(false)
+}
+
+func TestConnection_ListenParallelMessageListener(t *testing.T) {
+	if amqpURI() == "" {
+		t.Skip("Environment variable AMQP_URI not set")
+	}
+	c, err := Dial(amqpURI())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c.ch == nil {
+		t.Error("Expected chan got <nil>")
+	}
+	if cap(c.ch) == 0 {
+		t.Error("Expected buffered chan got cap=")
+	}
+	if got, want := c.MaxAttempts, defaultMaxAttempts; got != want {
+		t.Errorf("Expected MaxAttempts=%d, got=%d", want, got)
+	}
+	if got, want := c.Delay, defaultDelay; got != want {
+		t.Errorf("Expected MaxAttempts=%d, got=%d", want, got)
+	}
+	c.MaxAttempts = 1
+	c.Delay = 0
+
+	conn, err := amqp.Dial(amqpURI())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ch.Close()
+	tempQ, err := NamedReplyQueue(ch, randomString(10))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ch.QueueDelete(tempQ.Name, false, false, false)
+
+	consumer := new(dummyDeliveryConsumer)
+	listener, err := NewParallelMessageListener(tempQ.Name, 1, consumer)
+	go c.Listen(listener)
+
+	corrID := "XYZ"
+
+	ch.Publish("", tempQ.Name, false, false, amqp.Publishing{
+		CorrelationId: corrID,
+		Body:          []byte(corrID),
+	})
+
+	for {
+		if consumer.corrID != nil {
+			break
+		}
+	}
+	if got, want := *consumer.corrID, corrID; got != want {
+		t.Errorf("Expected CorrelationId='%s', got '%s'", want, got)
+	}
+	c.Close()
+	// wait for internal channels to close
+	time.Sleep(1 * time.Second)
+}
+
 func amqpURI() string { return os.Getenv("AMQP_URI") }
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
